8d75b9f3a2b06f5a0f39c71ff250e7bfb9425ee6
DmitryLasuta <lasutadima01@gmail.com>
2023-12-07 01:54:05 +0300

    feat: add a loading page

---
+++ app/products/[...slug]/loading.tsx
@@ -0,0 +1,77 @@
+import { ProductCardSkeletonGroup } from '@/components/ui'
+
+const TextSkeleton = () => {
+  return (
+    <div className="py-4 text-center lg:text-justify">
+      <p className="w-full h-6 animate-pulse bg-[#c7c7c7] mb-4 inline-block rounded"></p>
+      <p className="w-10/12 h-6 animate-pulse bg-[#c7c7c7] mb-4 inline-block rounded"></p>
+      <p className="w-10/12 h-6 animate-pulse bg-[#c7c7c7] mb-4 inline-block rounded"></p>
+      <p className="w-1/2 h-6 animate-pulse bg-[#c7c7c7] mb-4 inline-block rounded"></p>
+    </div>
+  )
+}
+
+const TitleSkeleton = () => {
+  return (
+    <div className="text-center">
+      <p className="w-1/2 h-10 animate-pulse bg-[#c7c7c7] mb-4 inline-flex rounded"></p>
+    </div>
+  )
+}
+
+export default function ProductDetailsLoadingPage() {
+  return (
+    <>
+      {/* Breadcrumbs */}
+      <div className="flex flex-wrap gap-4 items-center mb-4" role="status" aria-label="loading page elements">
+        <div className="animate-pulse bg-[#c7c7c7] w-20 lg:w-40 h-12 rounded"></div>
+        <span className="mx-3 inline-block">{'>>'}</span>
+        <div className="animate-pulse bg-[#c7c7c7] w-20 lg:w-40 h-12 rounded"></div>
+        <span className="mx-3 inline-block">{'>>'}</span>
+        <div className="animate-pulse bg-[#c7c7c7] w-20 lg:w-40 h-12 rounded"></div>
+      </div>
+      {/* Image block */}
+      <div role="status" aria-label="loading page elements">
+        <div className="mb-8 text-center flex flex-col items-center lg:flex-row md:items-start gap-4 xl:gap-8">
+          {/* Image */}
+          <div className="animate-pulse bg-[#616161] w-[250px] h-[300px] md:w-[650px] md:h-[500px] rounded"></div>
+          {/* Params panel */}
+          <div
+            className="text-left border-2 flex-1 w-full rounded h-fit px-2 py-4"
+            role="status"
+            aria-label="loading page elements"
+          >
+            {['Rating', 'Price', 'In stock'].map((param, index) => (
+              <p key={index} className="mb-2 flex items-center gap-2">
+                {param}: <span className="animate-pulse inline-block bg-[#c7c7c7] w-12 h-6 rounded"></span>
+              </p>
+            ))}
+            {/* Button */}
+            <div className="animate-pulse bg-[#616161] w-full h-9 rounded"></div>
+          </div>
+        </div>
+        {/* About product section */}
+        <div className="border-y-2 py-8">
+          <div className="lg:max-w-[90%] lg:mx-auto">
+            {/* title */}
+            <TitleSkeleton />
+            {/* Description */}
+            <TextSkeleton />
+          </div>
+        </div>
+      </div>
+      {/* Good to know */}
+      <div className="py-8 border-b-2" role="status" aria-label="loading page elements">
+        <TitleSkeleton />
+        <TextSkeleton />
+        <TextSkeleton />
+        <TextSkeleton />
+      </div>
+      {/* Related products */}
+      <div className="py-8" role="status" aria-label="loading page elements">
+        <TitleSkeleton />
+        <ProductCardSkeletonGroup count={8} />
+      </div>
+    </>
+  )
+}
--- app/products/[...slug]/page.tsx
+++ app/products/[...slug]/page.tsx
@@ -3,6 +3,7 @@ import { Button, Container, Section } from '@/components/ui/common'
 import { Breadcrumbs } from '@/components'
 import { DatabaseService } from '@/lib/services'
 import Image from 'next/image'
+import { Metadata } from 'next'
 import { ProductCardSkeletonGroup } from '@/components/ui'
 import dynamic from 'next/dynamic'
 import { notFound } from 'next/navigation'
@@ -12,11 +13,22 @@ const RelatedProducts = dynamic(() => import('@/components/ui/ProductList'), {
   loading: () => <ProductCardSkeletonGroup count={8} />,
 })
 
-export default async function ProductPage({ params }: { params: { slug: string[] } }) {
-  const { slug } = params
-  const [category, id] = slug
+export const revalidate = 60
 
-  const product = await new DatabaseService().getProductById(Number(id))
+const storeDB = new DatabaseService()
+export const generateMetadata = async ({ params }: { params: { slug: string[] } }): Promise<Metadata> => {
+  const [_, id] = params.slug
+  const product = await storeDB.getProductById(Number(id))
+  return {
+    title: `${product?.title} - ${product?.category}`,
+    description: product?.description,
+  }
+}
+
+export default async function ProductPage({ params }: { params: { slug: string[] } }) {
+  const [category, id] = params.slug
+
+  const product = await storeDB.getProductById(Number(id))
   if (!product) return notFound()
 
   const { category: productCategory, description, image, price, rating, title } = product
@@ -36,11 +48,11 @@ export default async function ProductPage({ params }: { params: { slug: string[]
       <figure>
         <div className="mb-8 text-center flex flex-col items-center lg:flex-row md:items-start gap-4 xl:gap-8">
           <Image
-            className="inline-block max-h-[400px] object-contain  bg-[white] rounded p-10 flex-1"
+            className="inline-block max-h-[450px] object-contain  bg-[white] rounded p-10 flex-1"
             src={image}
             alt={title}
             width={700}
-            height={300}
+            height={450}
           />
           <div className="text-left border-2 flex-1 w-full rounded h-fit px-2 py-4">
             <p className="mb-2">
@@ -54,10 +66,30 @@ export default async function ProductPage({ params }: { params: { slug: string[]
         <figcaption className="border-y-2 py-8">
           <div className="lg:max-w-[90%] lg:mx-auto">
             <h1 className="text-3xl text-center font-bold mb-4">{title}</h1>
-            <p className={`first-letter:uppercase text-justify text-lg`}>{description}</p>
+            <p className={`first-letter:uppercase text-center  lg:text-justify text-lg`}>{description}</p>
           </div>
         </figcaption>
       </figure>
+      <section className="py-8 border-b-2">
+        <h4 className="text-3xl text-center capitalize font-bold mb-4">good news</h4>
+        <div className="mx-auto max-w-[90%] text-center lg:text-justify">
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, voluptate molestiae! Laboriosam
+            excepturi exercitationem ipsa illum maxime eaque eum possimus minus quos, deleniti, consequuntur iusto
+            quaerat sunt suscipit sed iure accusamus, in modi beatae voluptatem. Soluta commodi hic nihil nisi.
+          </p>
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, voluptate molestiae! Laboriosam
+            excepturi exercitationem ipsa illum maxime eaque eum possimus minus quos, deleniti, consequuntur iusto
+            quaerat sunt suscipit sed iure accusamus, in modi beatae voluptatem. Soluta commodi hic nihil nisi.
+          </p>
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, voluptate molestiae! Laboriosam
+            excepturi exercitationem ipsa illum maxime eaque eum possimus minus quos, deleniti, consequuntur iusto
+            quaerat sunt suscipit sed iure accusamus, in modi beatae voluptatem. Soluta commodi hic nihil nisi.
+          </p>
+        </div>
+      </section>
       <Section title="Related Products">
         <RelatedProducts category={productCategory} count={4} orderByRating={true} withoutProduct={product} />
       </Section>
