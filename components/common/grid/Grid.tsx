ac4987056629bc29a2bf2d3570d41097ab3c99cb
DmitryLasuta <lasutadima01@gmail.com>
2023-12-14 01:33:25 +0300

    style: fix wrapping

--- components/common/grid/Grid.tsx
+++ components/common/grid/Grid.tsx
@@ -7,5 +7,9 @@ interface GridProps {
 }
 
 export const Grid = ({ children }: GridProps) => {
-  return <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">{children}</ul>
+  return (
+    <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 justify-items-center">
+      {children}
+    </ul>
+  )
 }
--- components/common/grid/GridItem.tsx
+++ components/common/grid/GridItem.tsx
@@ -1,3 +1,3 @@
 export const GridItem = ({ children }: { children: React.ReactNode }) => {
-  return <li className="max-w-[300px] md:w-full sm:mx-auto">{children}</li>
+  return <li className="w-[300px] md:w-full sm:mx-auto">{children}</li>
 }
